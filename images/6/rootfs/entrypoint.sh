44f4e044626bc3b1816806fdea2d528e26c8ee7b
Soner Sayakci <s.sayakci@shopware.com>
2023-04-26 07:18:20 +0200

    feat: add basic 6.5 compatibility

--- images/6/rootfs/entrypoint.sh
+++ images/6/rootfs/entrypoint.sh
@@ -3,7 +3,7 @@
 /usr/bin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --user=mysql &
 
 unset APP_ENV
-echo "APP_ENV=prod" > /var/www/shop/.env
+echo "APP_ENV=prod" > /var/www/shop/.env.local
 chown -R 1000 /var/www/shop/.env
 
 while ! mysqladmin ping --silent; do
@@ -16,19 +16,13 @@ else
   mysql -proot shopware -e "UPDATE sales_channel_domain set url = 'http://${VIRTUAL_HOST}/shop/public' where url = 'http://localhost/shop/public'"
 fi
 
-sudo -u www-data git clone https://github.com/FriendsOfShopware/FroshPlatformAdminer.git /var/www/shop/custom/plugins/FroshPlatformAdminer --depth=1 -b 0.3.4
 sudo -u www-data git clone https://github.com/shopwareLabs/SwagTestEnvironment.git /var/www/shop/custom/plugins/SwagTestEnvironment --depth=1
 
 rm -rf /var/www/shop/var/cache/* || true
 
-if sudo -E -u www-data /var/www/shop/bin/console store:download -p SwagI18nDutch; then
-    sudo -E -u www-data /var/www/shop/bin/console plugin:refresh
-    sudo -E -u www-data /var/www/shop/bin/console plugin:install -n --activate SwagI18nDutch
-else
-    sudo -E -u www-data /var/www/shop/bin/console store:download -p SwagLanguagePack
-    sudo -E -u www-data /var/www/shop/bin/console plugin:refresh
-    sudo -E -u www-data /var/www/shop/bin/console plugin:install -n --activate SwagLanguagePack
-fi
+sudo -E -u www-data /var/www/shop/bin/console store:download -p SwagLanguagePack
+sudo -E -u www-data /var/www/shop/bin/console plugin:refresh
+sudo -E -u www-data /var/www/shop/bin/console plugin:install -n --activate SwagLanguagePack
 
 
 sudo -E -u www-data /var/www/shop/bin/console plugin:install -n --activate FroshPlatformAdminer SwagTestEnvironment
